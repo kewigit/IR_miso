6b50d446687a16d0afba89ece9c7fe227db6277d
kewigit <45994964+kewigit@users.noreply.github.com>
2019-12-11 16:39:03 +0900

    IR Send
    Go APIから弾数を受け取る

--- IrRaspiSend.py
+++ IrRaspiSend.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # _*_ coding: utf-8 _*_
+"""
+curl -X GET http://192.168.x.x:9999/sendBullet?id=123\&bullet=XX
+"""
 
 import requests
 import RPi.GPIO as GPIO
@@ -7,22 +10,23 @@ import subprocess
 import time
 
 #def getbullet():
-# url = 'http://192.168.1.1:9999/recvBullet?id=123'
-# req =  requests.get(url)
-# bullet = req.json()
- #bullet["Bullet"]  # 仮弾数
-#print("Bullet",ammunition)
+url = 'http://192.168.1.171:9999/recvBullet?id=123'
+req =  requests.get(url)
+bullet = req.json()
+#bullet["Bullet"]  # 仮弾数
+print("Bullet",bullet)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 
 try:
-   ammunition = 5
+   ammunition = bullet["Bullet"]
+
    while True:
 
       if ammunition <= 0:
          print("play end")
-         # url = 'http://192.168.1.1:9999/sendNotice?id=123'
+         url = 'http://192.168.1.171:9999/sendNotice?id=123'
          exit(0)
 
       if GPIO.input(4) == GPIO.LOW:
