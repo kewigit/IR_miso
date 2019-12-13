d3e5505fe920945baef9bde51b16f3f562c2fa85
kewigit <45994964+kewigit@users.noreply.github.com>
2019-12-14 03:37:14 +0900

    IR 送信
    IR送信プログラムの言語をpythonからCに変更

--- IrRaspiSend.py
+++ IrRaspiSend.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python
 # _*_ coding: utf-8 _*_
 
+"""
+送信波形: NECリモコン 記録ファイル: recieves
+
+curl -X GET http://192.168.x.x:9999/sendBullet?id=123\&bullet=XX
+"""
+
 import requests
 import RPi.GPIO as GPIO
 import subprocess
 import time
 
-#def getbullet():
-# url = 'http://192.168.1.1:9999/recvBullet?id=123'
-# req =  requests.get(url)
-# bullet = req.json()
- #bullet["Bullet"]  # 仮弾数
-#print("Bullet",ammunition)
+url = 'http://192.168.1.171:9999/recvBullet?id=25004'
+req =  requests.get(url)
+bullet = req.json()
+print("Bullet",bullet)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 
 try:
-   ammunition = 5
-   while True:
+   ammunition = bullet["Bullet"]
 
+   while True:
       if ammunition <= 0:
          print("play end")
-         # url = 'http://192.168.1.1:9999/sendNotice?id=123'
+         url = 'http://192.168.1.171:9999/sendNotice?id=25007'
+         requests.get(url)
          exit(0)
 
       if GPIO.input(4) == GPIO.LOW:
          print("sw on")
-         cmd = "timeout 0.3 python3 irrp.py -p -g17 -f recieves Infrared"
+         #CHANGED: IR送信　言語をpythonからCに変更
+         cmd = "timeout 0.2 sudo ./wink 17"
          subprocess.call(cmd.split())
          print(cmd)
          ammunition -= 1
          print(ammunition)
 
-      time.sleep(0.5)
+      time.sleep(0.1)
 
 except KeyboardInterrupt:
+   print("\nCtl+C")
    pass
 
-#ピンをクリーンする
-GPIO.cleanup()
-#pi.set_mode(4, pigpio.OUTPUT)
-#pi.stop()
+GPIO.cleanup() #ピンをクリーンする
